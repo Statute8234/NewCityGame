32d46055ac3a05ee47500778d15f4d3cf523d6b1
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-06-06 15:03:29 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,7 @@
 import ursina
 from ursina import *
-from numpy import floor
 import time, random, sys
-from perlin_noise import PerlinNoise
-from ursina.shaders import lit_with_shadows_shader
-import GameObjects
+import Island, Menu
 
 current_time = time.time()
 random.seed(current_time)
@@ -45,8 +42,9 @@ def update_season():
         update_terrain_texture()
 
 def update_terrain_texture():
+    global weatherChance
     for entity in scene.entities:
-        if isinstance(entity, GameObjects.Ground):
+        if isinstance(entity, Island.Ground):
             if current_season == 'spring':
                 entity.change(current_season)
                 weatherChance = [('clear',0.8),('rain',0.27),('snow',0)]
@@ -81,12 +79,12 @@ def apply_weather_effects(weather):
                     scene.fog_density = (1, 50)
                     disable_rain()
                     disable_snow()
-                elif w == 'rain':
+                if w == 'rain':
                     sky.color = color.gray
                     scene.fog_density = (10, 50)
                     enable_rain()
                     disable_snow()
-                elif w == 'snow':
+                if w == 'snow':
                     sky.color = color.white
                     scene.fog_density = (5, 50)
                     enable_snow()
@@ -153,34 +151,6 @@ current_season_index = random.randint(0, len(seasons) - 1)
 current_season = seasons[current_season_index]
 start_time = time.time()
 
-# Create Island
-noise = PerlinNoise(octaves=random.randint(1, 6), seed=random.randint(0, sys.maxsize))
-freq = random.uniform(5, 30)
-amp = random.uniform(1, 15)
-terrain_radius = 30
-center_x = 0
-center_z = 0
-
-def is_within_circle(x, z, radius):
-    return sqrt((x - center_x) ** 2 + (z - center_z) ** 2) <= radius
-
-# island
-availible_spots = []
-def Island(position):
-    dome = GameObjects.Dome(subdivisions=terrain_radius)
-    for x in range(-terrain_radius, terrain_radius):
-        for z in range(-terrain_radius, terrain_radius):
-            if is_within_circle(position[0] + x, position[2] + z, terrain_radius):
-                ground = GameObjects.Ground(position=(position[0] + x, 0, position[2] + z))
-                ground.y = position[1] + floor(noise([ground.x / freq, ground.z / freq]) * amp)
-                ground.change(current_season)
-                availible_spots.append(ground.position)
-
-# place
-def place_building(position):
-    for item in availible_spots:
-        if item[0] == position[0] and item[2] == position[2]:
-            GameObjects.Building(position=(availible_spots[0][0],availible_spots[0][1] + 1,availible_spots[0][2]))
 # game
 app = Ursina()
 window.fullscreen = False
@@ -188,10 +158,9 @@ window.fps_counter.enabled = False
 window.cog_button.enabled = False
 window.show_ursina_splash = True
 window.icon = r'assets\city.png'
-main_menu = GameObjects.MainMenu()
 # Island
-Island(position=(0, 0, 0))
-place_building(position=(0, 0, 0))
+main_menu = Menu.MainMenu()
+playerIsland = Island.build_island(position= (0,0,0), current_season = current_season)
 # camera
 camera = EditorCamera()
 sun = DirectionalLight(shadow_map_resolution=(2048, 2048))
